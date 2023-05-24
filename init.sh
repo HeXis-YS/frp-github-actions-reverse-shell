86e7facd7fe072d23ed6001e1a730e89a60a248d
HeXis-YS <leonador@yeah.net>
2023-05-24 16:45:05 +0800

    Use TCP BBR by default

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@
 (sleep 300m && wall "Warning: The workflow will be closed within 1 Hour.") &
 (sleep 330m && wall "Warning: The workflow will be closed within 30 Minutes.") &
 (sleep 350m && wall "Warning: The workflow will be closed within 10 Minutes.") &
+sudo sysctl -w net.ipv4.tcp_congestion_control=bbr
