567f9910b30341d1cfaddc3e9899d5318064cfac
HeXis-YS <leonador@yeah.net>
2023-05-28 11:13:23 +0800

    Mount /tmp as tmpfs by default

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+# Warn when workflow is about to close
 (sleep 300m && wall "Warning: The workflow will be closed within 1 Hour.") &
 (sleep 330m && wall "Warning: The workflow will be closed within 30 Minutes.") &
 (sleep 350m && wall "Warning: The workflow will be closed within 10 Minutes.") &
+
+# Enable bbr by default
 sudo sysctl -w net.ipv4.tcp_congestion_control=bbr
+
+# Mount /tmp as tmpfs
+sudo mount -t tmpfs -o rw,nosuid,nodev tmpfs /tmp
