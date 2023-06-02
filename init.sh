f7e51c8667131fa02a64b1a525a2ff278c209148
HeXis-YS <leonador@yeah.net>
2023-06-02 18:10:24 +0800

    Merge mkswap.sh into init.sh

--- init.sh
+++ init.sh
@@ -9,3 +9,13 @@ sudo sysctl -w net.ipv4.tcp_congestion_control=bbr
 
 # Mount /tmp as tmpfs
 sudo mount -t tmpfs -o rw,nosuid,noatime,nodev,size=10G tmpfs /tmp
+
+# Make the whole /mnt as swap
+if [[ ${INIT_MKSWAP} == 'true' ]]; then
+    sudo swapoff -a
+    sudo rm -rf /mnt/*
+    sudo fallocate -l $(df --output=avail -B 1 . | tail -n 1) /mnt/swapfile
+    sudo chmod 600 /mnt/swapfile
+    sudo mkswap /mnt/swapfile
+    sudo swapon /mnt/swapfile
+fi
--- mkswap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo swapoff -a
-sudo rm -rf /mnt/*
-sudo fallocate -l $(df --output=avail -B 1 . | tail -n 1) /mnt/swapfile
-sudo chmod 600 /mnt/swapfile
-sudo mkswap /mnt/swapfile
-sudo swapon /mnt/swapfile
