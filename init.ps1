bf08cc28742e4f0d3bf4d0a315ec4cbd23a362a7
HeXis-YS <leonador@yeah.net>
2023-06-14 15:22:13 +0800

    Disable Windows Defender

--- init.ps1
+++ init.ps1
@@ -7,6 +7,15 @@ trap {
     Exit 1
 }
 
+# Disable Windows Defender
+./AdvancedRun.exe -oC:\Windows -p"AdvancedRun.exe" -y
+foreach($service in "WdNisSvc", "WinDefend", "Sense", "WdnisDrv", "wdfilter", "wdboot") {
+    AdvancedRun.exe /EXEFilename "C:\Windows\System32\net.exe" /CommandLine "STOP $service" /RunAs 8 /Run
+    AdvancedRun.exe /EXEFilename "C:\Windows\System32\sc.exe" /CommandLine "config $service start=disabled" /RunAs 8 /Run
+}
+AdvancedRun.exe /EXEFilename "C:\Windows\System32\reg.exe" /CommandLine "add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f" /RunAs 8 /Run /WaitProcess 1
+AdvancedRun.exe /EXEFilename "C:\Windows\System32\reg.exe" /CommandLine "add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiVirus /t REG_DWORD /d 1 /f" /RunAs 8 /Run /WaitProcess 1
+
 # disable password complexity requirements
 secedit /export /cfg c:\secpol.cfg
 (gc C:\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\secpol.cfg
