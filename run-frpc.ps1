69e71a99e549d126394c43071a898e20d13e59b3
HeXis-YS <leonador@yeah.net>
2023-08-07 13:50:00 +0800

    [Windows] Use frpc amd64_v3

--- run-frpc.ps1
+++ run-frpc.ps1
@@ -8,24 +8,10 @@ trap {
 }
 
 # Install frp
-mkdir -Force frp | Out-Null
-Push-Location frp
-
-if (!(Test-Path frps.exe)) {
+if (!(Test-Path frpc.exe)) {
     Write-Output 'Downloading frp...'
-    try {
-        (New-Object System.Net.WebClient).DownloadFile(
-            ((Invoke-WebRequest "https://api.github.com/repos/fatedier/frp/releases/latest" | ConvertFrom-Json).assets | Where-Object name -like "frp_*_windows_amd64.zip").browser_download_url,
-            "$PWD/frp.zip")
-    } catch {
-        (New-Object System.Net.WebClient).DownloadFile("https://github.com/fatedier/frp/releases/download/v0.50.0/frp_0.50.0_windows_amd64.zip", "$PWD/frp.zip")
-    }
-
-    Write-Output 'Extracting frp...'
-    tar xf frp.zip --strip-components=1
+    (New-Object System.Net.WebClient).DownloadFile("https://github.com/HeXis-YS/frp/releases/latest/download/frpc_windows_amd64_v3.exe", "$PWD/frpc.exe")
 }
-
-Pop-Location
 
 # When running in CI override the frpc tls files.
 if (Test-Path env:FRPC_TLS_CA_CERTIFICATE) {
@@ -43,4 +29,4 @@ if (Test-Path env:RUNNER_PASSWORD) {
 }
 
 Write-Output 'Running frpc...'
-./frp/frpc -c frpc-windows.ini 2>&1 | Select-Object {$_ -replace '[0-9\.]+:7000','***:7000'}; cmd /c exit 0
+./frpc -c frpc-windows.ini 2>&1 | Select-Object {$_ -replace '[0-9\.]+:7000','***:7000'}; cmd /c exit 0
