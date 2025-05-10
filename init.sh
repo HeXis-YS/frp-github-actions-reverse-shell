6672834b8ef40d3dab302ffd7f7a68b7d19ab27e
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2025-05-10 22:26:52 +0800

    [Ubuntu] Disable reserved blocks

--- init.sh
+++ init.sh
@@ -32,7 +32,7 @@ if [ "$VERSION_ID" == "24.04" ]; then
     umount /mnt
     modprobe brd rd_size=65536 max_part=1
     mke2fs -F -O journal_dev /dev/ram0
-    mke2fs -F -O ^resize_inode,has_journal,sparse_super2,fast_commit,orphan_file,extent,flex_bg,inline_data,bigalloc -E num_backup_sb=0 -J device=/dev/ram0 /dev/disk/cloud/azure_resource-part1
+    mke2fs -F -O ^resize_inode,has_journal,sparse_super2,fast_commit,orphan_file,extent,flex_bg,inline_data,bigalloc -E num_backup_sb=0 -J device=/dev/ram0 -m 0 /dev/disk/cloud/azure_resource-part1
     mount -o nodev,noatime,lazytime,journal_async_commit,nobarrier,noauto_da_alloc,commit=21600,data=writeback,inode_readahead_blks=4096 /mnt
 else
     tune2fs -O fast_commit /dev/disk/cloud/azure_resource-part1
