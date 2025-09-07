840d435f76a9dbe381dec4aa8463661bd565fe0f
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2025-09-07 22:59:10 +0800

    [Ubuntu] Optimizations from clr-power-tweaks

--- init_root.sh
+++ init_root.sh
@@ -9,14 +9,20 @@ ldconfig
 install -m 755 bin/* /usr/local/bin/
 
 # Disk tweaks
-echo -n 0 | tee \
-    /sys/class/block/sd[a-z]/queue/add_random \
-    /sys/class/block/sd[a-z]/queue/iostats \
-    /sys/class/block/sd[a-z]/queue/rotational
-echo -n none | tee \
-    /sys/class/block/sd[a-z]/queue/scheduler
-echo -n 2 | tee \
-    /sys/class/block/sd[a-z]/queue/rq_affinity
+pushd /sys/class/block
+echo none | tee \
+    sd[a-z]/queue/scheduler
+echo 0 | tee \
+    sd[a-z]/queue/add_random \
+    sd[a-z]/queue/iostats \
+    sd[a-z]/queue/rotational
+echo 2 | tee \
+    sd[a-z]/queue/rq_affinity
+echo 4096 | tee \
+    sd[a-z]/queue/nr_requests
+echo 1024 | tee \
+    sd[a-z]/queue/read_ahead_kb
+popd
 
 # Disable swap
 swapoff -a
@@ -49,52 +55,62 @@ mount -t tmpfs -o rw,nodev,noatime,nodiratime,lazytime,size=$(awk '/MemTotal/{pr
 
 # Sysctl
 sysctl -w \
-    net.ipv4.tcp_congestion_control=bbr \
+    kernel.core_pattern="|/usr/bin/false" \
+    kernel.randomize_va_space=0 \
+    kernel.sched_autogroup_enabled=0 \
+    kernel.unprivileged_bpf_disabled=1 \
     net.core.default_qdisc=fq \
-    net.ipv4.tcp_ecn=1 \
-    net.ipv6.conf.all.disable_ipv6=1 \
-    net.ipv6.conf.default.disable_ipv6=1 \
-    net.ipv6.conf.lo.disable_ipv6=1 \
-    net.ipv4.ip_forward=0 \
-    net.ipv4.conf.default.forwarding=0 \
-    net.ipv4.conf.all.forwarding=0 \
     net.core.netdev_max_backlog=16384 \
-    net.core.somaxconn=8192 \
     net.core.rmem_max=67108864 \
-    net.core.wmem_max=67108864 \
-    net.ipv4.tcp_rmem="4096 131072 67108864" \
-    net.ipv4.tcp_wmem="4096 131072 67108864" \
-    net.ipv4.udp_rmem_min=8192 \
-    net.ipv4.udp_wmem_min=8192 \
+    net.core.somaxconn=8192 \
+    net.core.wmem_max=16777216 \
+    net.ipv4.conf.all.accept_redirects=0 \
+    net.ipv4.conf.all.forwarding=0 \
+    net.ipv4.conf.all.rp_filter=1 \
+    net.ipv4.conf.all.secure_redirects=0 \
+    net.ipv4.conf.all.send_redirects=0 \
+    net.ipv4.conf.default.accept_redirects=0 \
+    net.ipv4.conf.default.forwarding=0 \
+    net.ipv4.conf.default.rp_filter=1 \
+    net.ipv4.conf.default.secure_redirects=0 \
+    net.ipv4.conf.default.send_redirects=0 \
+    net.ipv4.icmp_echo_ignore_all=1 \
+    net.ipv4.ip_forward=0 \
+    net.ipv4.tcp_congestion_control=bbr \
+    net.ipv4.tcp_ecn=1 \
     net.ipv4.tcp_fastopen=3 \
-    net.ipv4.tcp_max_syn_backlog=8192 \
-    net.ipv4.tcp_max_tw_buckets=2097152 \
-    net.ipv4.tcp_tw_reuse=1 \
     net.ipv4.tcp_fin_timeout=10 \
-    net.ipv4.tcp_slow_start_after_idle=0 \
-    net.ipv4.tcp_no_metrics_save=1 \
-    net.ipv4.tcp_keepalive_time=60 \
     net.ipv4.tcp_keepalive_intvl=10 \
     net.ipv4.tcp_keepalive_probes=6 \
-    net.ipv4.conf.default.rp_filter=1 \
-    net.ipv4.conf.all.rp_filter=1 \
-    net.ipv4.tcp_synack_retries=1 \
+    net.ipv4.tcp_keepalive_time=60 \
+    net.ipv4.tcp_max_syn_backlog=8192 \
+    net.ipv4.tcp_max_tw_buckets=2097152 \
+    net.ipv4.tcp_no_metrics_save=1 \
+    net.ipv4.tcp_rmem="4096 131072 67108864" \
+    net.ipv4.tcp_slow_start_after_idle=0 \
     net.ipv4.tcp_syn_retries=1 \
-    net.ipv4.conf.all.accept_redirects=0 \
-    net.ipv4.conf.default.accept_redirects=0 \
-    net.ipv4.conf.all.send_redirects=0 \
-    net.ipv4.conf.default.send_redirects=0 \
-    net.ipv4.conf.all.secure_redirects=0 \
-    net.ipv4.conf.default.secure_redirects=0 \
+    net.ipv4.tcp_synack_retries=1 \
+    net.ipv4.tcp_tw_reuse=1 \
+    net.ipv4.tcp_wmem="4096 131072 16777216" \
+    net.ipv4.udp_rmem_min=8192 \
+    net.ipv4.udp_wmem_min=8192 \
     net.ipv6.conf.all.accept_redirects=0 \
+    net.ipv6.conf.all.secure_redirects=0 \
     net.ipv6.conf.default.accept_redirects=0 \
-    net.ipv4.icmp_echo_ignore_all=1 \
+    net.ipv6.conf.default.secure_redirects=0 \
     net.ipv6.icmp.echo_ignore_all=1 \
-    vm.dirty_ratio=50 \
     vm.dirty_background_ratio=5 \
-    vm.vfs_cache_pressure=50 \
-    kernel.core_pattern="|/usr/bin/false" \
-    kernel.randomize_va_space=0
+    vm.dirty_ratio=50 \
+    vm.dirty_writeback_centisecs=1500 \
+    vm.extfrag_threshold=100 \
+    vm.mmap_min_addr=65536 \
+    vm.vfs_cache_pressure=50
+
+echo 30000 > /sys/kernel/mm/transparent_hugepage/khugepaged/scan_sleep_millisecs
+echo 0 > /sys/devices/virtual/graphics/fbcon/cursor_blink
+echo 0 > /sys/kernel/rcu_expedited
+echo 4000 > /sys/kernel/mm/ksm/sleep_millisecs
+echo 1000 > /sys/kernel/mm/ksm/pages_to_scan
 
 # Replace qdisc manually
 tc qdisc replace dev eth0 root fq
@@ -109,8 +125,8 @@ apt update
 apt install -y linux-modules-extra-$(uname -r) earlyoom
 systemctl start earlyoom
 modprobe zram
-echo -n zstd > /sys/block/zram0/comp_algorithm
-echo -n $(($(awk '/MemTotal/{print $2}' /proc/meminfo) * 2))K > /sys/block/zram0/disksize
+echo zstd > /sys/block/zram0/comp_algorithm
+echo $(($(awk '/MemTotal/{print $2}' /proc/meminfo) * 2))K > /sys/block/zram0/disksize
 mkswap /dev/zram0
 swapon -p 0 /dev/zram0
 sysctl -w  \
@@ -124,8 +140,9 @@ sysctl -w  \
 # bash -c 'echo "KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,sntrup761x25519-sha512@openssh.com" >> /etc/ssh/sshd_config.d/60-custom.conf'
 
 # Fix password login
-sed -i -e '/^PasswordAuthentication/s/^/# /' -e '/^KbdInteractiveAuthentication/s/^/# /' \
-    /etc/ssh/sshd_config /etc/ssh/sshd_config.d/*
+sed -i /etc/ssh/sshd_config /etc/ssh/sshd_config.d/* \
+    -e '/^PasswordAuthentication/s/^/# /' \
+    -e '/^KbdInteractiveAuthentication/s/^/# /'
 echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 echo "KbdInteractiveAuthentication yes" >> /etc/ssh/sshd_config
 systemctl restart ssh.socket
