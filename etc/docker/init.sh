3ea8ef23edb0001c4de0068fbdd0e599db81baee
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2025-09-15 11:38:19 +0800

    [Ubuntu] Use Debian sid as docker image

--- etc/docker/init.sh
+++ etc/docker/init.sh
@@ -1,26 +1,32 @@
-# Fix KVM permissions
-groupadd -r kvm -g $1 || groupmod -g $1 kvm
-gpasswd -a rhino kvm || true
-
 # Disable man-db processing
 dpkg-divert --divert /usr/bin/mandb.real --rename /usr/bin/mandb
 echo -e '#!/bin/sh\nexit 0' > /usr/bin/mandb
 chmod 755 /usr/bin/mandb
 
-# Install dependencies
 apt update
-apt upgrade -y
-apt autopurge -y
-apt install -y libeatmydata1 udev
-rm -rf /var/lib/apt/lists/*
 
 # Configure libs
+apt install -y libeatmydata1
 echo /usr/local/lib > /etc/ld.so.conf.d/00custom.conf
 echo /lib/$(uname -m)-linux-gnu/libeatmydata.so > /etc/ld.so.preload
 ldconfig
 
+# Add user
+apt install -y adduser sudo
+addgroup --gid $2 debian
+adduser --uid $1 --gid $2 --disabled-password --gecos "" debian
+usermod -aG sudo debian
+echo "debian ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+# Fix KVM permissions
+addgroup --gid $3 kvm || groupmod -g $3 kvm
+gpasswd -a debian kvm || true
+
+# Install dependencies
+apt install -y udev nano readline-common
+
 # Fix color terminal
-sed -i 's/#force_color_prompt/force_color_prompt/' /home/rhino/.bashrc
+sed -i 's/#force_color_prompt/force_color_prompt/' /home/debian/.bashrc
 
 # Self deletion
 rm -f $(realpath $0)
