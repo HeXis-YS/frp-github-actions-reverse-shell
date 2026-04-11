3a1f6d58443d6361209c56229c78ec3c94a12134
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2026-04-11 17:37:38 +0800

    Remove zlib-ng build steps from init script
    Removed zlib-ng build process from init.sh.

--- init.sh
+++ init.sh
@@ -1,34 +1,5 @@
 #!/usr/bin/bash
 
-source /usr/local/etc/action-shell/buildflags
-
-pushd /tmp
-
-# Build zlib-ng
-git clone -b stable --depth 1 --single-branch --no-tags https://github.com/zlib-ng/zlib-ng
-mkdir zlib-ng/build
-pushd zlib-ng/build
-cmake .. \
-    -DCMAKE_BUILD_TYPE=release \
-    -DCMAKE_C_COMPILER=clang \
-    -DCMAKE_C_FLAGS_RELEASE="$CLANG_CFLAGS" \
-    -DCMAKE_SHARED_LINKER_FLAGS="$CLANG_LDFLAGS" \
-    -DZLIB_COMPAT=ON \
-    -DZLIB_ENABLE_TESTS=OFF \
-    -DWITH_NATIVE_INSTRUCTIONS=ON \
-    -DNATIVE_ARCH_OVERRIDE="$ARCH_CFLAGS" \
-    -DWITH_RUNTIME_CPU_DETECTION=OFF \
-    -DWITH_GTEST=OFF \
-    -DBUILD_SHARED_LIBS=ON \
-    -DWITH_SANITIZER=OFF
-make -j$(nproc)
-sudo make install
-sudo ldconfig
-popd # zlib-ng/build
-rm -rf zlib-ng
-
-popd # /tmp
-
 # Fix color terminal
 sed -i 's/xterm-color/xterm|xterm-color/' ~/.bashrc
 
